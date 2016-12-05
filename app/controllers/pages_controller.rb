56b29fba293d7703815d470e424af572b88edcf2
Arthur Stefe <arthur.stefe@gmail.com>
2016-12-05 18:36:58 -0200

    Ajustes home e anunciante

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,13 @@
 class PagesController < ApplicationController
 
   def home
-    @ads = Ad.all
+    @ads = Ad.all.shuffle
     if user_signed_in?
       @columnist = Columnist.where("user_id = ?", current_user.id).first
       @subscriber = Subscriber.where("user_id = ?", current_user.id).first
     end
     @versicles = Versicle.where("show_day = ?", Date.today)
+    @ads_ban = Ad.all.order('created_at DESC limit 2').shuffle
+    @ads_body = Ad.all.order('created_at DESC limit 10').shuffle
   end
-
 end
