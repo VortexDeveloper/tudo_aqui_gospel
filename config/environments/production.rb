7b6986743743a19cbb46d12a4381d5243645a42a
= <=>
2017-03-06 14:19:00 -0300

    default url host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://tudoaquigospel.herokuapp.com/' }
   config.action_mailer.smtp_settings = {
     address: 'smtp.vortexdeveloper.com',
     port: 587,
